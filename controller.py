f8985d8d7bd2add60e02d2df1fcb9ea3dfcb2d96
Joseph Williams <89496887+coolguy761@users.noreply.github.com>
2023-12-08 23:04:27 -0500

    Updated controller.py

--- controller.py
+++ controller.py
@@ -1,20 +1,22 @@
 from pydub import AudioSegment
-import os
-from scipy.io import wavfile
 
 class AudioController:
     @staticmethod
     def process_audio(input_file_path):
         try:
             # Load audio file
-            audio = AudioSegment.from_file(input_file_path)
+            audio_data = AudioSegment.from_file(input_file_path)
 
-            # Convert to WAV, reduce channels to 1, and strip metadata
-            mono_wav = audio.set_channels(1)
-            output_file_path = "tempaudio.wav"
-            mono_wav.export(output_file_path, format="wav")
+            # Convert to mono if stereo
+            if audio_data.channels > 1:
+                audio_data = audio_data.set_channels(1)
 
-            return output_file_path
+            # Convert to WAV, and strip metadata
+            if not input_file_path.endswith('.wav'):
+                input_file_path = input_file_path.replace(input_file_path.split('.')[-1], 'wav')
+                audio_data.export(input_file_path, format='wav')
+
+            return input_file_path
 
         except Exception as e:
             raise RuntimeError(f"An error occurred: {str(e)}")
