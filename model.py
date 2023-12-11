904d087e679a92afae4f4058dc61c184413e4639
Khalee242 <151599422+Khalee242@users.noreply.github.com>
2023-12-10 23:02:10 -0500

    Update model.py for compute highest resonance and time

--- model.py
+++ model.py
@@ -11,17 +11,14 @@ class AudioModel:
 
     def find_target_frequency(self, freqs, target_band):
         if target_band == "high":
-            min_freq, max_freq = 1000, max(freqs)
+            min_freq, max_freq = 2000, max(freqs)
         elif target_band == "mid":
-            min_freq, max_freq = 500, 1000
+            min_freq, max_freq = 500, 2000
         elif target_band == "low":
             min_freq, max_freq = 0, 500
         else:
             raise ValueError("Invalid frequency band")
-        for x in freqs:
-            if min_freq <= x <= max_freq:
-                break
-        return x
+        return min_freq, max_freq
 
     def frequency_check(self, target_band):
         target_frequency = self.find_target_frequency(self.freqs, target_band)
@@ -31,15 +28,42 @@ class AudioModel:
         data_in_db_fun = 10 * np.log10(np.abs(data_for_frequency))
         return data_in_db_fun, self.t[:len(data_in_db_fun)]  # Adjust the time array length
 
-    def plot_spectogram(self, target_band):
-        spectrum, freqs, t, im = plt.specgram(self.data, Fs=self.sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+
+
+    def plot_spectrogram(self, target_band):
+        # Create a new figure for each spectrogram plot
+        plt.figure()
+
+        # Use find_target_frequency to get the appropriate frequency range
+        if target_band == "all":
+            min_freq, max_freq = 0, max(self.freqs)
+        else:
+            min_freq, max_freq = self.find_target_frequency(self.freqs, target_band)
+
+        # Plot the spectrogram with the updated frequency range
+        spectrum, freqs, t, im = plt.specgram(self.data, Fs=self.sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'), vmin=-40, vmax=40)
+
         plt.xlabel('Time (s)')
         plt.ylabel('Frequency (Hz)')
-        plt.title('Spectrogram')
+        plt.title(f'Spectrogram ({target_band} frequency range)')
         plt.colorbar(label='Intensity (dB)')
+
+        if target_band != "all":
+            # Manually select frequencies within the desired range
+            min_index = int(np.searchsorted(freqs, min_freq))
+            max_index = int(np.searchsorted(freqs, max_freq))
+            selected_spectrum = spectrum[min_index:max_index, :]
+
+            # Plot the spectrogram with the selected frequency range
+            extent = [t.min(), t.max(), freqs[min_index], freqs[max_index]]
+            plt.imshow(10 * np.log10(np.abs(selected_spectrum)), aspect='auto', origin='lower', cmap='autumn_r', extent=extent, vmin=-40, vmax=40)
+
+        else:
+            # Plot the spectrogram for all frequencies
+            plt.imshow(10 * np.log10(np.abs(spectrum)), aspect='auto', origin='lower', cmap='autumn_r', extent=[t.min(), t.max(), 0, max_freq], vmin=-40, vmax=40)
+
         plt.show()
 
-        print(f'The RT60 reverb time is not applicable for spectrogram.')
 
     def plot_waveform_simple(self):
         plt.figure()
@@ -49,3 +73,14 @@ class AudioModel:
         plt.title('Waveform')
         plt.grid()
         plt.show()
+
+    def compute_highest_resonance_frequency(self):
+        # Find the frequency with the highest amplitude in the spectrum
+        max_freq_index = np.argmax(np.abs(self.spectrum))
+        highest_resonance_frequency = self.freqs[max_freq_index]
+        return highest_resonance_frequency
+
+    def get_time_values(self):
+        total_seconds = self.t[-1]  # Get the total time in seconds
+        rounded_duration = round(total_seconds, 3)  # Round to three decimal places
+        return rounded_duration
